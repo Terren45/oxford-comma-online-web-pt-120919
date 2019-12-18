fc2ef44d21dfc2cd8b2758c5cc7126fdd487076a
Terren Culberson <culbersonterren@yahoo.com>
2019-12-18 19:46:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,10 @@ if array.length ==1
   return "#{array[0]}"
   elsif array.length == 2
    return array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
   new_last_array_item = "and #{array[-1]}"
   array.pop
   array.push(new_last_array_item)
- return array.join(" and ")
+ return array.join(" , ")
  end
 end 
