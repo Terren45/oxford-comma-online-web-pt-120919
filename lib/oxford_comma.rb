703f57e0642a140cbbaaa1892c04ee3e2c196ef4
Terren Culberson <culbersonterren@yahoo.com>
2019-12-18 20:02:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length ==1
   return "#{array[0]}"
   elsif array.length == 2
    return array.join(" and ")
-  elsif array.length >= 3
+  elsif array.length => 3
   array[-1] = "and #{array[-1]}"
  return array.join(" , ")
  end
